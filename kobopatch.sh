10c207e185ce5fc015e497edbc57276964025150
Yohann Leon <yohann@leon.re>
2023-08-01 02:44:23 +0900

    Add affiliate and change nm config

--- kobopatch.sh
+++ kobopatch.sh
@@ -2,28 +2,22 @@
 cd "$(dirname "$0")"
 rm -f "out/KoboRoot.tgz"
 case `uname -s` in
-    CYGWIN_NT*)
-        ./bin/koboptch-windows.exe
-        ;;
     Darwin)
-	    ./bin/kobopatch-darwin-64bit
-	    ;;
-    Linux)
 	    case `uname -m` in
-	        i?86)
-                ./bin/kobopatch-linux-32bit
-		        ;;
 	        x86_64)
-                ./bin/kobopatch-linux-64bit
+	        ./bin/kobopatch-darwin-64bit
 		        ;;
             arm*)
-                ./bin/kobopatch-linux-arm
+                ./bin/kobopatch-darwin-arm
                 ;;
             aarch64)
-                ./bin/kobopatch-linux-arm
+                ./bin/kobopatch-darwin-arm
                 ;;
             *)
                 echo "Unsupported architecture"
 	esac
+        ;;
+    *)
+        echo "Unsupported system"
 	;;
 esac
