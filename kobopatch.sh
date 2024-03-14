a3c3aff9a9b488670d54774c126137c3b7356022
Yohann Leon <yohann@leon.re>
2024-03-15 01:09:30 +0900

    Fix wrong overwrite for tailscale

--- kobopatch.sh
+++ kobopatch.sh
@@ -5,8 +5,8 @@ cd "$(dirname "$0")"
 if [ ! -f src/tailscale-${TAILSCALE_VERSION}-arm.tgz ]; then
     echo "Downloading tailscale_${TAILSCALE_VERSION}_arm.tgz from pkgs.tailscale.com ..."
     curl --progress-bar -o src/tailscale-${TAILSCALE_VERSION}-arm.tgz https://pkgs.tailscale.com/stable/tailscale_${TAILSCALE_VERSION}_arm.tgz
-    tar -xvf src/tailscale-${TAILSCALE_VERSION}-arm.tgz -C src
     rm -f "src/tailscale"
+    tar -xvf src/tailscale-${TAILSCALE_VERSION}-arm.tgz -C src
     mv src/tailscale_${TAILSCALE_VERSION}_arm src/tailscale
 fi
 
