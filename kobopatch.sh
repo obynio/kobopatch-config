dc3c2db99a75228fc28923659c3511f9b509cffa
Yohann Leon <yohann@leon.re>
2026-04-20 02:43:55 +0900

    Update execution to 4.38.23684

--- kobopatch.sh
+++ kobopatch.sh
@@ -13,30 +13,24 @@ fi
 mkdir -p out && rm -f "out/KoboRoot.tgz"
 case `uname -s` in
     Darwin)
-	    case `uname -m` in
-	        x86_64)
-	            ./bin/kobopatch-darwin-64bit
-		        ;;
-            arm*)
-                ./bin/kobopatch-darwin-arm
-                ;;
-            aarch64)
-                ./bin/kobopatch-darwin-arm
+	case `uname -m` in
+            arm64)
+                ./bin/kobopatch-darwin-arm64
                 ;;
             *)
                 echo "Unsupported architecture"
 	esac
     ;;
     Linux)
-	    case `uname -m` in
-	        x86_64)
+	case `uname -m` in
+	    x86_64)
                 ./bin/kobopatch-linux-64bit
-		        ;;
+		;;
             *)
                 echo "Unsupported architecture"
 	esac
     ;;
     *)
         echo "Unsupported system"
-	    ;;
+	;;
 esac
